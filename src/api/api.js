cc7acd23641a15648547d4d341d077409fb6dffa
jeff <yinjiefeng@91ruler.com>
2019-03-11 10:40:33 +0800

    new commit

---
+++ src/api/api.js
@@ -0,0 +1,16 @@
+const API = {
+    OSSUrl: 'http://47.99.200.198/service/aliyun-oss/getSign',
+    Login: '/user/login',
+    Logout: '/user/logout',
+    Welcome: '/welcome',
+    EditPageList: '/edit/pageList',
+    EditInsertData: '/edit/insert',
+    EditUpdateData:'/edit/update',
+    EditDeleteData:'/edit/delete',
+    AdminUserPageList:'/user/pageList',
+    AdminUserInsert:'/user/insert',
+    AdminUserUpdate:'/user/update',
+    AdminUserDelete:'/user/delete',
+}
+
+export default API
---
+++ src/components/ueditor/ueditor.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <script id="editor" type="text/plain" ></script>
+  </div>
+</template>
+
+<script>
+  import '../../../public/ueditor/ueditor.config.js'
+  import '../../../public/ueditor/ueditor.all.js'
+  import '../../../public/ueditor/lang/zh-cn/zh-cn.js'
+
+
+  export default {
+    name: "UEditor",
+    props: {
+      id: {
+          type: String
+      },
+      config: {
+          type: Object
+      },
+      form:  {
+          type: Object
+      },
+    },
+    data() {
+      return {
+        editor: null
+      }
+    },
+    mounted() {
+      //初始化UE
+      const _this = this;
+      this.editor = UE.delEditor("editor");
+      this.editor = UE.getEditor('editor',this.config);
+    },
+    destoryed() {
+      this.editor.destory();
+    },
+    methods:{
+      getUEContent: function(){
+       return this.editor.getContent();
+      },
+      getContentTxt: function(){
+        return this.editor.getContentTxt();
+      }
+    }
+  }
+</script>
